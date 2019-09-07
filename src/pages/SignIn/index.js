2989dc130904eade589d06afbcbf10e0cbd503e3
Tadeu <tadeu.tupiz@gmail.com>
2019-09-07 19:13:19 -0300

    🧠 Lib Unform to Handle Form + Form Validation

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
-// import { Container } from './styles';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um email válido')
+    .required('O email é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="Go Barber" />
 
-      <form>
-        <input type="email" placeholder="Seu email" />
-        <input type="password" placeholder="Sua senha" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu email" />
+        <Input name="password" type="password" placeholder="Sua senha" />
 
         <button type="submit">Acessar</button>
         <Link to="/register">Cadastrar-se</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,23 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
-// import { Container } from './styles';
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um email válido')
+    .required('O email é obrigatório'),
+  password: Yup.string()
+    .min(6, 'A senha deve conter no mínimo 6 caracteres')
+    .required('A senha é obrigatória'),
+});
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="Go Barber" />
 
-      <form>
-        <input type="text" placeholder="Seu Nome" />
-        <input type="email" placeholder="Seu Email" />
-        <input type="password" placeholder="Sua senha" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Seu Nome" />
+        <Input name="email" type="email" placeholder="Seu Email" />
+        <Input name="password" type="password" placeholder="Sua senha" />
 
         <button type="submit">Criar conta</button>
         <Link to="/">Voltar</Link>
-      </form>
+      </Form>
     </>
   );
 }
