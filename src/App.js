91bec60f9dcf299320fbbafe9b96b6d3c590d24d
Tadeu <tadeu.tupiz@gmail.com>
2019-09-08 15:09:54 -0300

    👤 Persisting storage with redux persist

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
@@ -12,15 +13,17 @@ import history from './services/history';
 import GlobalStyle from './styles/global';
 
 // import store
-import store from './store';
+import { store, persistor } from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
