dce7ad58581975fea1336f0f74f4a8f35be148b2
Tadeu <tadeu.tupiz@gmail.com>
2019-09-07 21:08:14 -0300

    💥 Login action handler with redux saga

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -1,7 +1,18 @@
-const INITIAL_STATE = {};
+import produce from 'immer';
 
-export default function auth(state = INITIAL_STATE, action) {
+const USER_INITIAL_STATE = {
+  token: null,
+  signed: false,
+  loading: false,
+};
+
+export default function auth(state = USER_INITIAL_STATE, action) {
   switch (action.type) {
+    case '@auth/SIGN_IN_SUCCESS':
+      return produce(state, draft => {
+        draft.token = action.payload.token;
+        draft.signed = true;
+      });
     default:
       return state;
   }
